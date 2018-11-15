5f2a2165225b4cdaac6ccabc21a412c50427b2bb
Goblok0 <43988726+Goblok0@users.noreply.github.com>
2018-11-15 15:28:54 +0100

    Universal CSV to JSON converter

---
+++ homework/week_3/convert.py
@@ -0,0 +1,26 @@
+#dataset: https://data.world/popculture/imdb-5000-movie-dataset
+import csv
+import json
+
+INPUT_CSV = "IMDB.csv"
+
+def convert():
+    tot_dict = {}
+    with open(INPUT_CSV, 'r') as input_file:
+        reader = csv.DictReader(input_file)
+
+
+        id = 0
+        for row in reader:
+            if row:
+                sub_dict = {}
+                for header in list(reader.fieldnames):
+                    sub_dict[header] = row[header]
+                    tot_dict[id] = sub_dict
+                    id += 1
+
+    with open('data.json', 'w') as outfile:
+        json.dump(tot_dict, outfile)
+
+if __name__ == "__main__":
+    convert()
